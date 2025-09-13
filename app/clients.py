b2d82d60187cecda61bdec15b137a1875dbbfccb
Shashank <shashankshashidhar73@gmail.com>
2025-09-13 11:56:41 +0530

    add more metadata

--- app/clients.py
+++ app/clients.py
@@ -54,13 +54,27 @@ class GitHubClient(BaseClient):
             response = await client.get(url)
             response.raise_for_status()
             user_data = response.json()
+            print(user_data)
             return {
                 "name": user_data.get("name") or user_data.get("login") or "N/A",
+                "node_id": user_data.get("node_id", "N/A"),
                 "profile_url": user_data.get("html_url") or "N/A",
+                'avatar_url': user_data.get("avatar_url") or "N/A",
                 "bio": user_data.get("bio") or "No bio provided.",
+                "type": user_data.get("type", "N/A"),
+                "company": user_data.get("company") or "N/A",
+                "location": user_data.get("location") or "N/A",
+                "email": user_data.get("email") or "N/A",
+                "blog": user_data.get("blog") or "N/A",
+                "twitter_username": user_data.get("twitter_username") or "N/A",
+                "created_at": user_data.get("created_at") or "N/A",
                 "followers": user_data.get("followers", 0),
                 "following": user_data.get("following", 0),
-                "repository_count": user_data.get("public_repos", 0),
+                "followers_url": user_data.get("followers_url", "N/A"),
+                "following_url": user_data.get("following_url", "N/A"),
+                "public_repos": user_data.get("public_repos", 0),
+                "public_gists": user_data.get("public_gists", 0),
+                "created_at": user_data.get("created_at") or "N/A",
             }
         except httpx.HTTPStatusError as e:
             logger.error(f"HTTP error fetching user data for {username}: {e.response.status_code} - {e.response.text}")
